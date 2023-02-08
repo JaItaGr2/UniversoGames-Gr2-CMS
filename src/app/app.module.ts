d2f0d4e58b9839d562e2b8fde338c86a9ae2fe9a
Simo Confa <simone97confalonieri@gmail.com>
2023-02-08 15:32:40 +0100

    Creazione Review modello e service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormReviewComponent } from './form-review/form-review.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormReviewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/model/review.ts
@@ -0,0 +1,18 @@
+export interface Review {
+    _id: string;
+    title: string;
+    publicationDate: string;
+    content: string;
+    score: number;
+    reviewerName: string;
+    imageUrls: string[];
+    reviewedGame: ReviewedGame;
+    __v: number;
+}
+
+export interface ReviewedGame {
+    id: string;
+    name: string;
+}
+
+export type NewReview = Omit<Review, '_id'>;

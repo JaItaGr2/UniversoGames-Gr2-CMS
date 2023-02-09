2be8a21de907eaf10fcffc139dcf1bd6d84f7390
Simo Confa <simone97confalonieri@gmail.com>
2023-02-09 17:56:15 +0100

    Correzione errore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormReviewComponent } from './form-review/form-review.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { FormNewsComponent } from './form-news/form-news.component';
 import { NuovaCategoriaComponent } from './nuova-categoria/nuova-categoria.component';
 import { NuovoVideogiocoComponent } from './nuovo-videogioco/nuovo-videogioco.component';

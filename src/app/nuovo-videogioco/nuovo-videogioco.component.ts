94a1aad468e3332b9c85608a5ad10d25f462d536
Simo Confa <simone97confalonieri@gmail.com>
2023-02-15 20:51:32 +0100

    Correzione errori merge

--- src/app/nuovo-videogioco/nuovo-videogioco.component.ts
+++ src/app/nuovo-videogioco/nuovo-videogioco.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { VideogiochiService } from '../service/videogiochi.service';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -8,9 +8,9 @@ import { map } from 'rxjs';
 @Component({
   selector: 'app-nuovo-videogioco',
   templateUrl: './nuovo-videogioco.component.html',
-  styleUrls: ['./nuovo-videogioco.component.css']
+  styleUrls: ['./nuovo-videogioco.component.css'],
 })
-export class NuovoVideogiocoComponent implements OnInit{
+export class NuovoVideogiocoComponent implements OnInit {
   form: FormGroup = new FormGroup({
     title: new FormControl(''),
     category: new FormControl(''),
@@ -19,116 +19,112 @@ export class NuovoVideogiocoComponent implements OnInit{
     softwareHouse: new FormControl(''),
     publisher: new FormControl(''),
     numberOfPlayers: new FormControl(),
-    languages: new FormGroup
-    ({
-        voice: new FormArray([
-          new FormControl(''),
-        ]),
-        text: new FormArray([
-          new FormControl(''),
-        ]),
+    languages: new FormGroup({
+      voice: new FormArray([new FormControl('')]),
+      text: new FormArray([new FormControl('')]),
     }),
     coverImage: new FormControl(''),
   });
-  
+
   isEditMode = false;
-  
-  idVideogiocoDaModificare='';
 
+  idVideogiocoDaModificare = '';
 
-  constructor(private videogiochiService: VideogiochiService, private router: Router,private route: ActivatedRoute ) {}
+  constructor(
+    private videogiochiService: VideogiochiService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
       this.idVideogiocoDaModificare = params['id'];
-      if(this.idVideogiocoDaModificare){
+      if (this.idVideogiocoDaModificare) {
         this.isEditMode = true;
-        this.videogiochiService.getVideogioco(this.idVideogiocoDaModificare).pipe(
-        map((val: Videogioco) =>{
-          console.log('ok');
-          console.log(val);
-          return this.form = new FormGroup({
-            title: new FormControl(val.title),
-            category: new FormControl(val.category),
-            releaseDate: new FormControl(val.releaseDate),
-            genre: new FormControl(val.genre),
-            softwareHouse: new FormControl(val.softwareHouse),
-            publisher: new FormControl(val.publisher),
-            numberOfPlayers: new FormControl(val.numberOfPlayers),
-            languages: new FormGroup({
-                voice: new FormArray(
-                 val.languages.voice.map((step) => new FormControl(step))
-                ),
-                text: new FormArray(
-                  val.languages.text.map((step) => new FormControl(step))
-                ),
-              }),
-            coverImage: new FormControl(val.coverImage),
-                });
-              }
-            )
-            ).subscribe(console.log);
-            }
-          });
-  };
+        this.videogiochiService
+          .getVideogioco(this.idVideogiocoDaModificare)
+          .pipe(
+            map((val: Videogioco) => {
+              console.log('ok');
+              console.log(val);
+              return (this.form = new FormGroup({
+                title: new FormControl(val.title),
+                category: new FormControl(val.category),
+                releaseDate: new FormControl(val.releaseDate),
+                genre: new FormControl(val.genre),
+                softwareHouse: new FormControl(val.softwareHouse),
+                publisher: new FormControl(val.publisher),
+                numberOfPlayers: new FormControl(val.numberOfPlayers),
+                languages: new FormGroup({
+                  voice: new FormArray(
+                    val.languages.voice.map((step) => new FormControl(step))
+                  ),
+                  text: new FormArray(
+                    val.languages.text.map((step) => new FormControl(step))
+                  ),
+                }),
+                coverImage: new FormControl(val.coverImage),
+              }));
+            })
+          )
+          .subscribe(console.log);
+      }
+    });
+  }
 
-  get languagesFormGroup() : FormGroup{
+  get languagesFormGroup(): FormGroup {
     return this.form.get('languages') as FormGroup;
   }
 
-  get voiceFormArray() : FormArray{
+  get voiceFormArray(): FormArray {
     return this.form.get('languages.voice') as FormArray;
   }
 
-  get textFormArray() : FormArray{
+  get textFormArray(): FormArray {
     return this.form.get('languages.text') as FormArray;
   }
 
-  onAddVoice(){
-    this.voiceFormArray.push(
-       new FormControl(''),
-      );
+  onAddVoice() {
+    this.voiceFormArray.push(new FormControl(''));
   }
 
-  onRemoveVoice(index: number){
+  onRemoveVoice(index: number) {
     this.voiceFormArray.removeAt(index);
   }
 
-  onAddText(){
-    this.textFormArray.push(
-      new FormControl(''),
-    );
+  onAddText() {
+    this.textFormArray.push(new FormControl(''));
   }
 
-  onRemoveText(index: number){
+  onRemoveText(index: number) {
     this.textFormArray.removeAt(index);
   }
 
-  onSubmit(){
-    if (this.form.invalid){
+  onSubmit() {
+    if (this.form.invalid) {
       alert('Attenzione, compilare i campi obbligatori');
       return;
     }
 
-  let formResponse = this.form.getRawValue();
-  formResponse.__v = 0;
+    let formResponse = this.form.getRawValue();
+    formResponse.__v = 0;
 
-  if (this.isEditMode){
-    formResponse._id =this.idVideogiocoDaModificare;
-    console.log(formResponse);
-    this.videogiochiService.updateVideogioco(formResponse).subscribe(() => {
-      this.videogiochiService.getVideogioco(this.idVideogiocoDaModificare);
-    });
-  } else {
-    console.log(formResponse);
-    this.videogiochiService.addVideogioco(formResponse).subscribe(() => {
-      this.videogiochiService.getVideogiochi();
-    });
-  }
- 
-  this.form.reset();
-  this.isEditMode = false;
-  this.idVideogiocoDaModificare = '';
-  this.router.navigateByUrl('/');
+    if (this.isEditMode) {
+      formResponse._id = this.idVideogiocoDaModificare;
+      console.log(formResponse);
+      this.videogiochiService.updateVideogioco(formResponse).subscribe(() => {
+        this.videogiochiService.getVideogioco(this.idVideogiocoDaModificare);
+      });
+    } else {
+      console.log(formResponse);
+      this.videogiochiService.addVideogioco(formResponse).subscribe(() => {
+        this.videogiochiService.getVideogiochi();
+      });
+    }
+
+    this.form.reset();
+    this.isEditMode = false;
+    this.idVideogiocoDaModificare = '';
+    this.router.navigateByUrl('/');
   }
 }
